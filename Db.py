b1ecf5153eb392fe360067b8a5a97a1b2ac1dbde
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2015-09-22 15:33:31 -0400

    added pay dues

--- Db.py
+++ Db.py
@@ -1,13 +1,16 @@
 from google.appengine.ext import db
 #Table for users
 class Person(db.Model):
-	email = db.StringProperty(required=True)
-	created = db.DateTimeProperty(auto_now_add=True)
+    firstName = db.StringProperty(required=True)
+    lastName = db.StringProperty(required=True)
+    email = db.StringProperty(required=True)
+    dues_paid = db.BooleanProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
 class Gear(db.Model):
-	number = db.StringProperty(required=True)
-	description = db.TextProperty(required=True)
-	holder = db.ReferenceProperty(Person)
-	holderName = db.StringProperty(required=False)
-	returnDate = db.DateTimeProperty()
-	created = db.DateTimeProperty(auto_now_add=True)
-	size = db.FloatProperty(required=False)
+    number = db.StringProperty(required=True)
+    description = db.TextProperty(required=True)
+    holder = db.ReferenceProperty(Person)
+    holderName = db.StringProperty(required=False)
+    returnDate = db.DateTimeProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    size = db.FloatProperty(required=False)
